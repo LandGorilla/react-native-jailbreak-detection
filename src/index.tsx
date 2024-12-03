2c3d9df4f6d7520dfd2dabb6d8762bb591341848
FernandoGallardo <fernando@landgorilla.com>
2024-12-03 08:06:22 -0500

    feat(android): add android implementation

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-jailbreak-detection' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  Platform.select({
+    ios: "- You have run 'pod install'\n",
+    android: '- You have rebuilt the app after installing the package\n',
+    default: '',
+  }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
@@ -17,6 +21,11 @@ const JailbreakDetection = NativeModules.JailbreakDetection
       }
     );
 
+/**
+ * Checks if the device is jailbroken (rooted).
+ *
+ * @returns {Promise<boolean>} A promise that resolves to true if the device is jailbroken, false otherwise.
+ */
 export function isJailbroken(): Promise<boolean> {
   return JailbreakDetection.isJailbroken();
 }
